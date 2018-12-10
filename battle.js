4e31240f8a82e42c1a2fb4e558457ba0f6671a7d
ElliotWood13 <elliotreecewood@gmail.com>
2018-12-10 15:08:29 +0000

    bits

--- battle.js
+++ battle.js
@@ -1,17 +1,37 @@
-let turn = true;
+let turn;
 let playerOneHealth = 100;
 let playerTwoHealth = 100;
 let character; 
 
+const moveOne = document.getElementsByClassName("move1");
+const moveTwo = document.getElementsByClassName("move2");
+const moveThree = document.getElementsByClassName("move3");
+const moveFour = document.getElementsByClassName("move4");
 
-let randomNumber = Math.floor(Math.random());
+ 
 
-if (randomNumber > 0.5 && turn === true) {
-    playerOneHealth += 10; 
-    turn === false;
-} else if (randomNumber > 0.5 && turn === false) {
-    playerTwoHealth += 10;
-    turn === true;
+
+
+function randomNumber() {
+    return Math.floor(Math.random() * 10);
+
+    if (randomNumber() > 7 && turn === true) {
+        playerOneHealth += 10; 
+        turn === false;
+    } else if (randomNumber() > 7 && turn === false) {
+        playerTwoHealth += 10;
+        turn === true;
+    }
 }
+
+
+
+
+
+
+
+
+
+
 
 module.exports = {battle};
