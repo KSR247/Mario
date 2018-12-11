05e1bc0373b990500afe207a6f2b249227427aa7
mycheung85 <mycheung85@gmail.com>
2018-12-11 17:22:21 +0000

    created character and player instances in index.js

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+
 const Character = require("./classes/character");
 const Player = require("./classes/player");
 
-let mario = new Character("mario", "greet");;
-let yoshi = new Character("yoshi", "eat");
+// new Characters
 
-let characterArr = [mario, yoshi];
+let mario = new Character('mario', 'cape')
+let marioMoves = ['Jump', 'Pipe-clean']
+mario.pickMoves(marioMoves)
 
-let elliot = new Player("elliot", characterArr);
+
+let yoshi = new Character('yoshi', 'fireball');
+let yoshiMoves = ['dodge', 'kick']
+yoshi.pickMoves(yoshiMoves);
+
+
+// new players 
+let elliot = new Player("Elliot");
+elliot.pickCharacter(mario)
+
+
+let kas = new Player('Kas', yoshi);
+kas.pickCharacter(yoshi)
+
+// console.log(elliot._pick[0]._moves)
+// console.log(kas)
+// console.log(kas._pick[0]._moves)
