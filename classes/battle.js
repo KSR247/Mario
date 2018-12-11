a3ff4df40c272f8b6369217955c9dba0c2934dbb
ElliotWood13 <elliotreecewood@gmail.com>
2018-12-11 15:10:16 +0000

    move1 sorted

--- classes/battle.js
+++ classes/battle.js
@@ -1,23 +1,34 @@
-let turn;
-let playerOneHealth = 100;
-let playerTwoHealth = 100;
+const moveOne = document.querySelector('.move1');
+const moveTwo = document.querySelector('.move2');
+const moveThree = document.querySelector('.move3');
+const moveFour = document.querySelector('.move4');
+
+let turn = true;
+let playerOneHealth = 0;
+let playerTwoHealth = 0;
+let playerDamage;
 let character;
 
-const moveOne = document.getElementsByClassName("move1");
-const moveTwo = document.getElementsByClassName("move2");
-const moveThree = document.getElementsByClassName("move3");
-const moveFour = document.getElementsByClassName("move4");
+let health1 = document.querySelector('.healthBarOne');
+let health2 = document.querySelector('.healthBarTwo');
 
-function randomNumber() {
-  return Math.floor(Math.random() * 10);
-
-  if (randomNumber() > 7 && turn === true) {
-    playerOneHealth += 10;
-    turn === false;
-  } else if (randomNumber() > 7 && turn === false) {
-    playerTwoHealth += 10;
-    turn === true;
-  }
+function attackDamage(min, max) {
+  return Math.floor(Math.random() * (max-min) + min)
 }
 
-module.exports = battle;
+moveOne.addEventListener("click", attackOne = () => {
+  let playerDamage = attackDamage(10, 15);
+    if (turn) {
+      playerTwoHealth += playerDamage;
+      health2.style.width = (playerTwoHealth + '%');
+    } else if (turn === false) {
+      playerOneHealth += playerDamage;
+      health1.style.width = playerOneHealth; 
+    }
+  });
+
+
+
+
+
+// module.exports = battle;
